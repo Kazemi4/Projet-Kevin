a263695c879750cb6833169b1f99e2466a2190f8
Nelo <kevinpplle@gmail.com>
2019-11-20 10:45:13 +0100

    created project

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from "../assets/images/logo.svg"
+import { Link } from "react-router-dom";
+
+import "./Header.css"
+
+const Header = () => {
+    return (
+            <header className="header-header">
+        <img src={logo} className="header-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <Link  className="header-link" to="/"> Home </Link>
+        <Link  className="header-link" to="/characters">Characters </Link>
+      </header>
+    )
+}
+
+export default Header
